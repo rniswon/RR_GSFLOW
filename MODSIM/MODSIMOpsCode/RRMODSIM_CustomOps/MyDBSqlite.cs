8a8f969555d41eb8139624bd831254b763785f5b
RTI\etriana <etriana@rti.org>
2021-04-21 00:02:39 -0600

    New MODSIM custom code for streamlined operations - works with original Mendocino configuration. Incluyes new min flows and capactities algorithms.

---
+++ MODSIM/MODSIMOpsCode/RRMODSIM_CustomOps/MyDBSqlite.cs
@@ -0,0 +1,338 @@
+﻿/*
+ * --------------------------------------------------------------------------------
+ * Project:         WaterALLOC
+ * Namespace:       RTI.WRMD.WaterALLOC.Class
+ * Class:           SqliteHelper1
+ * Description:     <DESCRIPTION>
+ * Author:          anuragsrivastav@rti.org
+ * Date:            January 01, 2018 - March 31, 2018
+ * Note:            <NOTES>
+ * Revision History:
+ * Name:            Date:           Description:
+ * 
+ * 
+ * --------------------------------------------------------------------------------
+ */
+
+using System;
+using System.IO;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SQLite;
+
+namespace RTI.CWR.MMS_Support
+{
+    public class MyDBSqlite : IDisposable
+    {
+        public string dbFile { get; }
+        private string ConnectionString { get; set; }
+        private bool _version4Plus { get; set; }
+        private SQLiteTransaction _sqltransaction { get; set; }
+        private SQLiteConnection _sqlconnection { get; set; }
+
+
+        public MyDBSqlite()
+        {  
+            
+        }
+
+        public MyDBSqlite(string dbfile)
+        {
+            //dbFile = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), dbfile);
+            ConnectionString = GetSqLiteConnectionString(dbfile);
+        }
+
+        private string GetSqLiteConnectionString(string dbFileName)
+        {
+            SQLiteConnectionStringBuilder conn = new SQLiteConnectionStringBuilder
+            {
+                DataSource = dbFileName,
+                Version = 3,
+                FailIfMissing = true,
+            };
+            conn.Add("Compress", true);
+
+            return conn.ConnectionString;
+        }
+
+       private bool IsTableExist(string tablename)
+        {
+            bool isexist = false;
+            using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
+            {
+                try
+                {
+                    c.Open();
+                    string sql = "SELECT name FROM sqlite_master WHERE type = 'table'";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
+                    {
+                        SQLiteDataReader r = cmd.ExecuteReader();
+                        while (r.Read())
+                        {
+                            if (r[0].ToString() == tablename)
+                            {
+                                isexist = true;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                   Console.WriteLine("ERROR [DATABASE]: " + ex.Message);
+                }
+                finally
+                {
+                    c.Close();
+                }
+            }
+            return isexist;
+        }
+        
+        public int ExecuteQuery(string sql)
+        {
+            ////using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
+            ////{
+                int rowid=0;
+                try
+                {
+                    CheckDatabaseConnection();
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, _sqlconnection))
+                    {
+                        cmd.Transaction = _sqltransaction;
+                        rowid = cmd.ExecuteNonQuery();
+                        rowid = (int)_sqlconnection.LastInsertRowId;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    CommitTransaction();
+                }
+                return rowid;
+            //}
+        }
+
+        public object ExecuteScalar(string sql)
+        {
+            using (SQLiteConnection c = new SQLiteConnection(ConnectionString))
+            {
+                object m_value = null;
+                try
+                {
+                    c.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, c))
+                    {
+                        SQLiteDataReader r = cmd.ExecuteReader();
+                        while (r.Read())
+                        {
+                            m_value = r[0];
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message + "\n" + ex.StackTrace.ToString());
+                }
+                finally
+                {
+                    c.Close();
+                }
+                return m_value;
+            }
+        }
+      
+        private void CheckDatabaseConnection()
+        {
+            if (_sqlconnection == null || _sqlconnection.State != ConnectionState.Open)
+            {
+                _sqlconnection = new SQLiteConnection(ConnectionString);
+                _sqlconnection.Open();
+                //custom functions definition
+                _sqlconnection.BindFunction(new SQLiteFunctionAttribute("Power", 1, FunctionType.Scalar),
+                    (Func<object[], object>)((object[] args) => PowerFunction((object[])args[1])),
+                    null);
+            }
+
+            if (_sqltransaction == null)
+            {
+                _sqltransaction = _sqlconnection.BeginTransaction();
+            }
+            return;
+        }
+
+        object PowerFunction(object[] args)
+        {
+            var arg1 = (double)args[0];
+            var arg2 = (double)args[1];
+            return Math.Pow(arg1, arg2);
+        }
+
+
+        public DataTable GetTableFromDB(string sql, string tableName)
+        {
+            DataTable settingstable = new DataTable(tableName);
+
+            try
+            {
+                CheckDatabaseConnection();
+                using (SQLiteCommand cmd = new SQLiteCommand(sql, _sqlconnection))
+                {
+                    SQLiteDataReader r = cmd.ExecuteReader();
+                    while (r.Read())
+                    {
+                        if (settingstable.Columns.Count == 0)
+                        {
+                            for (int i = 0; i < r.FieldCount; i++)
+                            {
+                                settingstable.Columns.Add(r.GetName(i), r.GetFieldType(i));
+                            }
+                        }
+                        object[] rowValues = new object[r.FieldCount];
+                        r.GetValues(rowValues);
+                        settingstable.Rows.Add(rowValues);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR [DATABASE]: " + ex.Message);
+            }
+            finally
+            {
+                CommitTransaction();
+            }
+
+            return settingstable;
+        }
+
+        public bool UpdateTableFromDB(DataTable m_Table)
+        {
+            try
+            {
+                CheckDatabaseConnection();
+                // store ts pattern values
+                foreach (DataRow m_row in m_Table.Rows)
+                {
+                    string sql = "INSERT OR REPLACE INTO " + m_Table.TableName + " (";
+                    for (int i = 0; i < m_Table.Columns.Count; i++)
+                    {
+                        if (i > 0) sql += " ,";
+                        sql += m_Table.Columns[i].ColumnName;
+                    }
+                    sql += ") VALUES (";
+                    for (int i = 0; i < m_Table.Columns.Count; i++)
+                    {
+                        if (i > 0) sql += " ,";
+                        if (IsNumeric(m_Table.Columns[i]))
+                        {
+                            if (DBNull.Value.Equals(m_row[i]))
+                            {
+                                sql += "NULL";
+                            }
+                            else
+                            {
+                                sql += m_row[i].ToString();
+                            }
+                        }
+                        else if (m_Table.Columns[i].DataType.ToString().Contains("Date"))
+                        {
+                            DateTime m_date = (DateTime)m_row[i];
+                            sql += "'" + m_date.ToString("yyyy-MM-dd HH:MM:ss") + "'";
+                        }
+                        else
+                        {
+                            sql += "'" + m_row[i].ToString() + "'";
+                        }
+                    }
+                    sql += ");";
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, _sqlconnection))
+                    {
+                        cmd.Transaction = _sqltransaction;
+                        cmd.ExecuteNonQuery();                        
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR [DATABASE]: " + ex.Message);
+                return false;
+            }
+            finally
+            {
+                CommitTransaction();
+            }
+            return true;
+        }
+
+        public bool IsNumeric(DataColumn col)
+        {
+            if (col == null)
+                return false;
+            // Make this const
+            var numericTypes = new[] { typeof(Byte), typeof(Decimal), typeof(Double),
+        typeof(Int16), typeof(Int32), typeof(Int64), typeof(SByte),
+        typeof(Single), typeof(UInt16), typeof(UInt32), typeof(UInt64)};
+            return numericTypes.Contains(col.DataType);
+        }
+
+        public void CommitTransaction()
+        {
+            if (_sqltransaction != null && _sqltransaction.Connection != null)
+            {
+                _sqltransaction.Commit();
+                _sqltransaction = null;
+            }
+
+            if (_sqlconnection != null && _sqlconnection.State == ConnectionState.Open)
+            {
+                _sqlconnection.Close();
+                _sqlconnection = null;
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                    if (_sqltransaction != null)
+                    {
+                        _sqlconnection.Close();
+                        _sqltransaction.Dispose();
+                    }
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~SqliteHelper1() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+
+    }
+}
---
+++ MODSIM/MODSIMOpsCode/RRMODSIM_CustomOps/RRResCustomOps.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Csu.Modsim.ModsimIO;
+using Csu.Modsim.ModsimModel;
+
+using System.IO;
+using System.Data;
+
+using RTI.CWR.MMS_Support;
+
+namespace RTI.CWR.RRModel
+{
+    class Program
+    {
+        public static Model m_Model = new Model();
+        private static string fileName;
+
+        private static Dictionary<string, double[]> modelFactors;
+        private static string dataDBPath;
+
+        static void Main(string[] args)
+        {
+
+            m_Model.Init += OnInitialize;
+            m_Model.IterBottom += OnIterationBottom;
+            m_Model.IterTop += OnIterationTop;
+            m_Model.Converged += OnIterationConverge;
+            m_Model.End += OnFinished;
+            m_Model.OnMessage += OnMessage;
+            m_Model.OnModsimError += OnError;
+
+            fileName = args[0];
+            dataDBPath = args[1];
+            XYFileReader.Read(m_Model, fileName);
+
+            int run = Modsim.RunSolver(m_Model);
+
+            if (run == 0)
+            {
+                Console.WriteLine("Finished MODSIM run!");
+            }
+                    
+            Console.ReadKey();
+        }
+        
+        private static int AccuracyConversion(long value)
+        {
+            int newValue = (int)(value/m_Model.ScaleFactor);
+            return newValue;
+        }
+
+        private static Node LMendocino;
+        private static Link pillsburyIndex;
+        private static Link pillsburyStorage;
+        private static Link westForkFlows;
+        private static Link westForkMaxRelease;
+        private static Link hopelandGage;
+        private static MinFlowCalculator LMendoMinFlow;
+        private static int storageState=0;
+        private static ReleaseCapacity LMendoRelease;
+
+        private static void OnInitialize()
+        {
+            InitilizeVariables();
+            //min release clases
+            LMendoMinFlow = new MinFlowCalculator(dataDBPath, "Mendo_MinFlow_1610QTUCP", ref m_Model);
+            LMendocino = m_Model.FindNode("LMendocino");
+            pillsburyIndex = m_Model.FindLink("PillsburyIndex");
+            pillsburyStorage = m_Model.FindLink("PillsburyStorage");
+
+            //Mendocino Releases (controlled and uncontrolled)
+            LMendoRelease = new ReleaseCapacity(dataDBPath, "MendocinoReleases", ref m_Model);
+
+
+            //West Fork Flow
+            westForkFlows = m_Model.FindLink("WestForkFlow");
+            westForkMaxRelease = m_Model.FindLink("WestForkReleaseMax");
+            hopelandGage = m_Model.FindLink("Hopland_Gage");
+        }
+
+        private static void InitilizeVariables()
+        {
+            modelFactors = new Dictionary<string, double[]>();
+            //Monthly Loss factors
+            double[] lossFact = new double[12];
+            lossFact[0] = 0.1;
+            lossFact[1] = 0.2;
+            lossFact[2] = 0.3;
+            lossFact[3] = 0.4;
+            lossFact[4] = 0.5;
+            lossFact[5] = 0.6;
+            lossFact[6] = 0.7;
+            lossFact[7] = 0.8;
+            lossFact[8] = 0.8;
+            lossFact[9] = 0.8;
+            lossFact[10] = 0.8;
+            lossFact[11] = 0.8;
+            // The dictionary kwy corresponds to the link name in the MODSIM Network
+            modelFactors.Add("CLoss_Link", lossFact);
+        }
+
+        private static void OnIterationTop()
+        {
+
+            if (m_Model.mInfo.Iteration == 0)
+            {
+                DateTime thisDate = m_Model.TimeStepManager.Index2Date(m_Model.mInfo.CurrentModelTimeStepIndex, TypeIndexes.ModelIndex);
+                
+                //foreach (string key in modelFactors.Keys)
+                //{
+                //    Link l = m_Model.FindLink(key);
+                //    if(l!=null)l.m.loss_coef = modelFactors[key][mon - 1];
+                //}
+
+                //Set operation flows -
+                storageState = LMendoMinFlow.GetStorageState(m_Model, LMendocino.mnInfo.start, pillsburyStorage.mlInfo.flow);
+
+
+                int YearTypeFlag = (int)(pillsburyIndex.mlInfo.flow / m_Model.ScaleFactor);
+                if (YearTypeFlag == 1 || YearTypeFlag == 4)
+                {
+                    LMendoMinFlow.AssignMinFlowsToNodes(m_Model.mInfo.CurrentModelTimeStepIndex, thisDate, storageState.ToString());
+                }
+                else if (YearTypeFlag == 2 || YearTypeFlag == 3)
+                    // YearTypeFlag: 2 & 3
+                    // Min flow table uses columns 2 and 3 to store the values per node.
+                    LMendoMinFlow.AssignMinFlowsToNodes(m_Model.mInfo.CurrentModelTimeStepIndex, YearTypeFlag.ToString());
+                else
+                    LMendoMinFlow.AssignMinFlowsToNodes(m_Model.mInfo.CurrentModelTimeStepIndex, thisDate, "1");
+
+            }
+
+            //West Fork flow - Release Check
+            if ((westForkFlows != null) && (westForkMaxRelease != null))
+            {
+                //reservoir releases cannot exceed 25 cfs if flow in west fork are > 2500 cfs and flows at Hopland are > 8000 cfs
+                double maxFlow_cfs = 2500 * 1.98347;
+                //If (westForkFlows.mlInfo.flow > maxFlow_cfs * accuracyFactor And hopelandGage.mlInfo.flow > 8000 * 1.98347 * accuracyFactor) And m_Model.mInfo.Iteration > 0 Then
+                if ((westForkFlows.mlInfo.flow > maxFlow_cfs * m_Model.ScaleFactor) && (m_Model.mInfo.Iteration > 0))
+                {
+                    // 25 cfs in acre-feet per day
+                    westForkMaxRelease.mlInfo.hi = (long)Math.Round((49.5867769 * m_Model.ScaleFactor),0);
+                }
+                else
+                    westForkMaxRelease.mlInfo.hi = m_Model.defaultMaxCap;
+
+            }
+            //Mendocino Releases (Controlled and Uncontrolled)
+            //LMendoRelease.SetReleaseCapacity(m_Model.mInfo.CurrentModelTimeStepIndex, LMendocino, interpolate: true,);
+
+        }
+
+        private static void OnMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void OnError(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void OnIterationBottom()
+        {
+        }
+
+        private static void OnIterationConverge()
+        {
+        }
+
+        private static void OnFinished()
+        {
+        }
+    }
+}
