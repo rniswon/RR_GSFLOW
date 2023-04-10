1685a256654838c26f2ebb2adb523427825c5022
Adera <sadera@usgs.gov>
2023-04-10 11:34:40 -0700

    updated current_version to run 20230410_01

--- GSFLOW/scripts/manual_calib.py
+++ GSFLOW/scripts/manual_calib.py
@@ -16,7 +16,7 @@ import geopandas
 
 script_ws = os.path.abspath(os.path.dirname(__file__))
 repo_ws = os.path.join(script_ws, "..", "..")
-model_ws = os.path.join(repo_ws, "GSFLOW", "scratch", "20230324_01")
+model_ws = os.path.join(repo_ws, "GSFLOW", "scratch", "20230406_04")
 
 # # ag fields shapefile
 # ag_fields_file = (repo_ws, "GSFLOW", "scratch", "run_posproc_scripts", "GSFLOW", "worker_dir_ies", "results", "plots", "gsflow_inputs", "ag_frac.shp")
@@ -38,13 +38,15 @@ gsflow_control = os.path.join(model_ws, "GSFLOW", "worker_dir_ies", "gsflow_mode
 
 # ---- Read in -------------------------------------------------####
 
-# # load modflow
-# mf_ws = os.path.join(model_ws, "GSFLOW", "worker_dir_ies", "gsflow_model_updated", "windows")
-# #mf = flopy.modflow.Modflow.load(r"rr_tr.nam", model_ws = mf_ws, load_only = ['DIS', 'BAS6', 'UPW', 'MNW2', 'SFR', 'LAK'])
-# mf = flopy.modflow.Modflow.load(r"rr_tr.nam", model_ws = mf_ws, load_only = ['DIS', 'BAS6', 'UPW', 'SFR', 'LAK'])
+# load modflow
+mf_ws = os.path.join(model_ws, "GSFLOW", "worker_dir_ies", "gsflow_model_updated", "windows")
+#mf = flopy.modflow.Modflow.load(r"rr_tr.nam", model_ws = mf_ws, load_only = ['DIS', 'BAS6', 'UPW', 'MNW2', 'SFR', 'LAK'])
+#mf = flopy.modflow.Modflow.load(r"rr_tr.nam", model_ws = mf_ws, load_only = ['DIS', 'BAS6', 'UPW', 'SFR', 'LAK'])
+mf = flopy.modflow.Modflow.load(r"rr_tr.nam", model_ws = mf_ws, load_only = ['DIS', 'BAS6', 'LAK'])
 
-# load gsflow
-gs = gsflow.GsflowModel.load_from_file(control_file=gsflow_control)
+
+# # load gsflow
+# gs = gsflow.GsflowModel.load_from_file(control_file=gsflow_control)
 
 # # read in riparian zone shapefile
 # riparian_zone_5_6_9_12_13 = pd.read_csv(riparian_zone_file, sep=',')
@@ -52,27 +54,28 @@ gs = gsflow.GsflowModel.load_from_file(control_file=gsflow_control)
 
 
 
-# # ---- Update LAK -------------------------------------------####
-#
-# # load lak array and lakebed leakance
-# lakarr = mf.lak.lakarr.array[:,:,:,:]
-# bdlknc = mf.lak.bdlknc.array[:,:,:,:]
-#
-# # update lakebed leakance for lake 12
+# ---- Update LAK -------------------------------------------####
+
+# load lak array and lakebed leakance
+lakarr = mf.lak.lakarr.array[:,:,:,:]
+bdlknc = mf.lak.bdlknc.array[:,:,:,:]
+
+# update lakebed leakance for lake 12
 # bdlknc_lake_12 = 100
-# mask = lakarr == 12
-# bdlknc[mask] = bdlknc_lake_12
-#
-# # export lake file
-# cc = {0: bdlknc[0]}
-# cc = Transient3d(mf, mf.modelgrid.shape, np.float32, cc, "bdlknc_")
-# mf.lak.bdlknc = cc
-# lak_file = os.path.join(model_ws, "GSFLOW", "worker_dir_ies", "gsflow_model_updated", "modflow", "input", mf.lak.file_name[0])
-# mf.lak.fn_path = lak_file
-# mf.lak.write_file()
-#
-#
-#
+bdlknc_lake_12_factor = 0.5
+mask = lakarr == 12
+bdlknc[mask] = bdlknc[mask] * bdlknc_lake_12_factor
+
+# export lake file
+cc = {0: bdlknc[0]}
+cc = Transient3d(mf, mf.modelgrid.shape, np.float32, cc, "bdlknc_")
+mf.lak.bdlknc = cc
+lak_file = os.path.join(model_ws, "GSFLOW", "worker_dir_ies", "gsflow_model_updated", "modflow", "input", mf.lak.file_name[0])
+mf.lak.fn_path = lak_file
+mf.lak.write_file()
+
+
+
 # # ---- Update Kh and Kv -------------------------------------------####
 #
 # # set hk and vk mult factors
@@ -283,43 +286,84 @@ gs = gsflow.GsflowModel.load_from_file(control_file=gsflow_control)
 
 
 
-# ---- Update PRMS param: runs 4-6 -------------------------------------------####
+# # ---- Update PRMS param: runs 4-6 -------------------------------------------####
+#
+# # set mult_factor
+# mult_factor = 30
+#
+# # set months to change
+# months_to_change = [5,6,7,8,9]
+#
+# # get hru_subbasin
+# hru_subbasin = gs.prms.parameters.get_values("hru_subbasin")
+# #subbasins_to_change = list(range(4,22)) # mask of subbasins 4-21
+# subbasins_to_change = [4,5,6,7,8,9,10,11,12,13]
+# #subbasins_to_change = [20]
+# mask_subbasin = np.isin(hru_subbasin, subbasins_to_change)
+#
+# # # get ag_frac
+# # ag_frac = gs.prms.parameters.get_values("ag_frac")
+# # mask_not_ag = ag_frac == 0  # mask not ag
+#
+# # get jh_coef and split by month
+# jh_coef = gs.prms.parameters.get_values("jh_coef")
+# jh_coef_list = np.split(jh_coef, 12)
+# for i, df, in enumerate(jh_coef_list):
+#
+#     if i+1 in months_to_change:
+#
+#         # runs 4-6
+#         df[mask_subbasin] = df[mask_subbasin] * mult_factor
+#         jh_coef_list[i] = df
+#
+#
+# # concat
+# jh_coef = np.concatenate(jh_coef_list)
+#
+# # export updated param file
+# gs.prms.parameters.set_values("jh_coef", jh_coef)
+# gs.prms.parameters.write()
 
-# set mult_factor
-mult_factor = 0.25
-
-# set months to change
-months_to_change = [5,6,7,8,9]
-
-# get hru_subbasin
-hru_subbasin = gs.prms.parameters.get_values("hru_subbasin")
-#subbasins_to_change = list(range(4,22)) # mask of subbasins 4-21
-#subbasins_to_change = [4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,21]
-subbasins_to_change = [20]
-mask_subbasin = np.isin(hru_subbasin, subbasins_to_change)
-
-# get ag_frac
-ag_frac = gs.prms.parameters.get_values("ag_frac")
-mask_not_ag = ag_frac == 0  # mask not ag
-
-# get jh_coef and split by month
-jh_coef = gs.prms.parameters.get_values("jh_coef")
-jh_coef_list = np.split(jh_coef, 12)
-for i, df, in enumerate(jh_coef_list):
-
-    if i+1 in months_to_change:
-
-        # runs 4-6
-        df[mask_subbasin] = df[mask_subbasin] * mult_factor
-        jh_coef_list[i] = df
 
 
-# concat
-jh_coef = np.concatenate(jh_coef_list)
-
-# export updated param file
-gs.prms.parameters.set_values("jh_coef", jh_coef)
-gs.prms.parameters.write()
+# # ---- Update PRMS param -------------------------------------------####
+#
+# # set mult_factor
+# mult_factor = 5
+#
+# # set months to change
+# months_to_change = [5,6,7,8,9]
+#
+# # get hru_subbasin
+# hru_subbasin = gs.prms.parameters.get_values("hru_subbasin")
+# #subbasins_to_change = list(range(4,22)) # mask of subbasins 4-21
+# subbasins_to_change = [4,5,6,7,8,9,10,11,12,13]
+# #subbasins_to_change = [20]
+# mask_subbasin = np.isin(hru_subbasin, subbasins_to_change)
+#
+# # get ag_frac
+# ag_frac = gs.prms.parameters.get_values("ag_frac")
+# mask_ag = ag_frac == 1  # mask ag
+#
+# # get jh_coef and split by month
+# jh_coef = gs.prms.parameters.get_values("jh_coef")
+# jh_coef_list = np.split(jh_coef, 12)
+# for i, df, in enumerate(jh_coef_list):
+#
+#     if i+1 in months_to_change:
+#
+#         # runs
+#         df[mask_subbasin & mask_ag] = df[mask_subbasin & mask_ag] * mult_factor
+#         #df[mask_subbasin] = 0.1
+#         jh_coef_list[i] = df
+#
+#
+# # concat
+# jh_coef = np.concatenate(jh_coef_list)
+#
+# # export updated param file
+# gs.prms.parameters.set_values("jh_coef", jh_coef)
+# gs.prms.parameters.write()
 
 
 
